b34ce2383340d5949c0238ddd0e03b40b804c741
Diego Romero Mora <diego.romero@solera.com>
2024-05-07 15:25:33 -0600

    addinf changes

--- 3unificacionDeIndice.py
+++ 3unificacionDeIndice.py
@@ -1,7 +1,6 @@
 import json
 
 #Indice
-# Leer el contenido de los archivos JSON
 with open('indice.json', 'r') as file1:
     json1 = json.load(file1)
 
@@ -14,11 +13,9 @@ def replace_locators(obj):
             if locator in value:
                 obj["Locators"][locator_index] = value[locator]
 
-# Aplicar la función a cada objeto del índice
 for item in json1:
     replace_locators(item)
     
 
-# Guardar el resultado en un nuevo archivo JSON
 with open('resultAdoFinal.json', 'w') as result_file:
     json.dump(json1, result_file, indent=4)
--- 4changeToFinalIndex.py
+++ 4changeToFinalIndex.py
@@ -1,14 +1,11 @@
 import json
 
-# Nombre del archivo JSON de entrada y salida
 input_file = 'resultAdoFinal.json'
 output_file = 'output_data.json'
 
-# Leer el archivo JSON de entrada
 with open(input_file, 'r') as file:
     initial_data = json.load(file)
 
-# Procesar los datos para ajustar la estructura al nuevo formato
 new_data = []
 for item in initial_data:
     new_item = {
@@ -16,7 +13,7 @@ for item in initial_data:
         "Content": item["Content"],
         "ApplicableTo": {
             "Keyword": item["Title"], #{item["URL"]}
-            "URL": f"https://ot1-qae-web.usdc.roadnet.com/{item['PageOb']}",  # Agrega la URL correspondiente
+            "URL": f"https://ot1-qae-web.usdc.roadnet.com/{item['PageOb']}",  
             "Locators": item["Locators"],
             "Language": "Robot Framework"
         },
@@ -24,7 +21,6 @@ for item in initial_data:
     }
     new_data.append(new_item)
 
-# Escribir los datos ajustados en un nuevo archivo JSON
 with open(output_file, 'w') as file:
     json.dump(new_data, file, indent=4)
 
